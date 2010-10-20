3788107edb827c7f3414a8b73ff0e2d2366a1115
Michael Strüder <mikezter@gmail.com>
2010-10-20 21:06:59 +0200

    Add Array#sum and Array#mean core_ext

--- array.rb
+++ array.rb
@@ -4,4 +4,13 @@ class Array
     each { |i| hash[yield(i)] ||= (array << i) }
     array
   end
+
+  def sum
+    inject(nil) { |sum,x| sum ? sum + x : x }
+  end
+
+  def mean
+    sum / size
+  end
+
 end
