d509db3c6e67bf4718844032a472ccfd4a2ee766
Hans Hasselberg <hans.hasselberg@kaeuferportal.de>
2010-11-05 15:06:32 +0100

    add deep_transform to HashWithIndifferentAccess

---
+++ indifferent_access.rb
@@ -0,0 +1,11 @@
+class HashWithIndifferentAccess
+  def self.deep_transform hash
+    a = HashWithIndifferentAccess.new(hash)
+    a.each do |k, v|
+      if v.is_a? Hash
+        v = deep_transform(v)
+      end
+    end
+    a
+  end
+end
