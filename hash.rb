9f1a22d61669abf64fdf3fa3a3b332823ecfaf99
Hans Hasselberg <hans.hasselberg@kaeuferportal.de>
2010-11-08 09:20:56 +0100

    add deep_merge to hash
    deep_merge merges included hashes:
    a = {:a => 1, :b => {:c => 2}}
    b = {:b => {:d => 3}}
    
    a.merge(b)
    => {:a => 1, :b => {:c => d3}}
    a.deep_merge(b)
    => {:a => 1, :b => { :c => 2, :d => 3}}

--- hash.rb
+++ hash.rb
@@ -14,5 +14,22 @@ class Hash
     end
     return default(key)
   end
+
+  def deep_merge(other_hash)
+    a = self.dup
+    other_hash.each do |k, v|
+      if v.is_a? Hash
+        if a.has_key? k
+          a[k] = a[k].deep_merge(v)
+        else
+          a[k] = v
+        end
+      else
+        a[k] = v
+      end
+    end
+    a
+  end
+
 end
 
