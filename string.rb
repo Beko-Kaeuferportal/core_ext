3effff14cdc403b066696f55cbbd633950cee2db
Michael Strüder <mikezter@gmail.com>
2010-11-25 14:29:33 +0100

    callname for string

--- string.rb
+++ string.rb
@@ -4,5 +4,17 @@ class String
   def truncate_at_word chars=100, ending="..."
     sub(/^(.{#{chars}}[\w.]*)(.*)/m){ $2.empty? ? $1 : $1 + ending}
   end
+
+  # returns a url/ascii/old-systems/non-utf8 friendly representation of self
+  def callname
+    @_callname ||= dup.
+      gsub(/[Ää]+/i,'ae').
+      gsub(/[Üü]+/i,'ue').
+      gsub(/[Öö]+/i,'oe').
+      gsub(/[ß]+/i,'ss').
+      downcase.
+      gsub(/[^a-z0-9]+/i, '-').
+      gsub(/(^[-]+|[-]+$)/, '')
+  end
 end
 
