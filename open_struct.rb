c4aba1a1606a12afe60cdbd63ef502b4a03bdc86
Michael Strüder <mikezter@gmail.com>
2011-01-25 16:48:09 +0100

    dont give object_id

--- open_struct.rb
+++ open_struct.rb
@@ -1,3 +1,3 @@
 # monkeypatch openstruct to get the method id working for openstructs
 # which get an id as attribute
-OpenStruct.__send__(:define_method, :id) { @table[:id] || self.object_id }
+OpenStruct.__send__(:define_method, :id) { @table[:id] }
