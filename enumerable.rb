be9c2b52c89a192320075efbb13e37e483862f07
Johannes A Kaefer <jak4@gmx.net>
2011-09-20 00:09:17 +0200

    refactore as to not collide with rails symbolize_keys method

--- enumerable.rb
+++ enumerable.rb
@@ -5,15 +5,15 @@ module Enumerable
 
   # symbolize a hash recursivly
   # if we encounter an array, we will also call symbolize for every hash or array found
-  def symbolize_keys
+  def symbolize_keys_rec
     if is_a?(Hash)
       inject({}) do |options, (key, value)|
-        options[(key.to_sym rescue key) || key] = (value.is_a?(Hash)||value.is_a?(Array)) ? value.symbolize_keys : value
+        options[(key.to_sym rescue key) || key] = (value.is_a?(Hash)||value.is_a?(Array)) ? value.symbolize_keys_rec : value
         options
       end
     elsif is_a?(Array)
       inject([]) do |options, value|
-        options << ((value.is_a?(Hash)||value.is_a?(Array)) ? value.symbolize_keys : value)
+        options << ((value.is_a?(Hash)||value.is_a?(Array)) ? value.symbolize_keys_rec : value)
         options
       end
     else
